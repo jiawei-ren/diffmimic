cece6e369e6d08e673f12a6d9764207330be66c2
jiawei-ren <jiawei011@e.ntu.edu.sg>
2023-04-12 12:31:22 +0800

    update visualization tool

--- visualize.py
+++ visualize.py
@@ -1,24 +1,26 @@
 import numpy as np
+import os
 from brax import envs
 from brax.io import html
 import streamlit.components.v1 as components
-from diffmimic.utils.io import deserialize_qp, serialize_qp
 import streamlit as st
+from diffmimic.utils.io import deserialize_qp, serialize_qp
 from diffmimic.mimic_envs import register_mimic_env
+
 register_mimic_env()
 
-st.title('DiffMimic - Visualization Tool')
+st.title('DiffMimic - Visualization')
+with st.expander("Readme"):
+    st.markdown(
+        'Input modes:'
+        '\n- File Path: input the local file path to the .npy trajectory file.'
+        '\n- Experiment Log: select trajectories by browsing experiment logs.'
+        '\n- Direct Upload: directly upload the .npy trajectory file.')
 
-uploaded_file = st.file_uploader("Choose a file")
-rollout_traj = None
-action_traj = None
 
-if uploaded_file is not None:
-    rollout_traj = np.load(uploaded_file)
-
-if rollout_traj is not None:
+def show_rollout_traj(rollout_traj, tag):
     if len(rollout_traj.shape) == 3:
-        seed = st.slider('Random seed', 0, rollout_traj.shape[1]-1, 0)
+        seed = st.slider(f'Random seed ({tag})', 0, rollout_traj.shape[1] - 1, 0)
         rollout_traj = rollout_traj[:, seed]
     if rollout_traj.shape[-1] == 182:
         system_cfg = 'humanoid'
@@ -27,7 +29,6 @@ if rollout_traj is not None:
     else:
         system_cfg = 'swordshield'
 
-    init_qp = deserialize_qp(rollout_traj[0])
     rollout_qp = [deserialize_qp(rollout_traj[i]) for i in range(rollout_traj.shape[0])]
     rollout_traj = serialize_qp(deserialize_qp(rollout_traj))
 
@@ -37,3 +38,37 @@ if rollout_traj is not None:
         reference_traj=rollout_traj,
     )
     components.html(html.render(env.sys, rollout_qp), height=500)
+
+
+def main():
+    tab_file_path, tab_experiment_log, tab_direct_upload = st.tabs(["File Path", "Experiment Log", "Direct Upload"])
+
+    with tab_file_path:
+        file_path = st.text_input('Path to trajectory')
+        if os.path.isfile(file_path):
+            with open(file_path, 'rb') as f:
+                rollout_traj = np.load(f)
+                show_rollout_traj(rollout_traj, 'FP')
+        else:
+            st.warning('Please input a valid path', icon="⚠️")
+
+    with tab_experiment_log:
+        try:
+            exp_name = st.selectbox('Experiment name', tuple(os.listdir('logs')))
+            all_files = sorted([x for x in os.listdir(os.path.join('logs', exp_name)) if x.endswith(".npy")])
+            file_name = st.selectbox('File name', tuple(all_files))
+            with open(os.path.join('logs', exp_name, file_name), 'rb') as f:
+                rollout_traj = np.load(f)
+                show_rollout_traj(rollout_traj, 'EL')
+        except:
+            st.warning('Please try other input modes', icon="⚠️")
+
+    with tab_direct_upload:
+        uploaded_file = st.file_uploader("Upload a trajectory")
+        if uploaded_file is not None:
+            rollout_traj = np.load(uploaded_file)
+            show_rollout_traj(rollout_traj, 'DU')
+
+
+if __name__ == '__main__':
+    main()
